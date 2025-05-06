28c0cb0ee58b4d6d31bcbe43f61895e6f93ac80a
MRLIGHT02 <saifali42020@gmail.com>
2025-05-06 16:30:48 +0530

    added the sql

---
+++ TodoListApplication/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoListApplication.DataFile;
+
+namespace TodoListApplication
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<UserData> UserDatas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserData>().HasData(
+
+                );
+        }
+    }
+}

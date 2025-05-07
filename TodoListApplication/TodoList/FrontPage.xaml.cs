3d48a837658bdcf0fcca8a80bd0f4be16690c375
MRLIGHT02 <saifali42020@gmail.com>
2025-05-07 14:43:01 +0530

    add file

--- TodoListApplication/TodoList/FrontPage.xaml.cs
+++ TodoListApplication/TodoList/FrontPage.xaml.cs
@@ -1,9 +1,20 @@
+using TodoListApplication.DataFile;
+
 namespace TodoListApplication.TodoList;
 
 public partial class FrontPage : ContentPage
 {
+	
+
 	public FrontPage()
 	{
 		InitializeComponent();
+		UserData userData = new UserData();
+
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        userData
+    }
 }

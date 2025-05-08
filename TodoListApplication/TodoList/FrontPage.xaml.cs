36fd2adb02bfc11e44a1d22b917fb148abc46f6a
MRLIGHT02 <saifali42020@gmail.com>
2025-05-08 17:09:27 +0530

    added

--- TodoListApplication/TodoList/FrontPage.xaml.cs
+++ TodoListApplication/TodoList/FrontPage.xaml.cs
@@ -13,7 +13,7 @@ public partial class FrontPage : ContentPage
         InitializeComponent();
         DataCollection = new ObservableCollection<DataFromTheUser>();
         BindingContext = this;
-        
+
     }
 
     //private ObservableCollection<DataFromTheUser> Getdata() => new ObservableCollection<DataFromTheUser>(
@@ -21,24 +21,31 @@ public partial class FrontPage : ContentPage
     //    {
     //        new DataFromTheUser{ItemData=itemofentery.Text}
     //    });
+
+    /// <summary>
+    /// This is item adding button 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void AddItemButtonClicked(object sender, EventArgs e)
-    {   
-     
+    {
+
         var newItem = new DataFromTheUser { ItemData = itemofentery.Text?.Trim() };
-         DataCollection.Add(newItem);
-       
+        DataCollection.Add(newItem);
 
-        
-      
-            
 
-        
-        
+
+
+
     }
-
+    /// <summary>
+    /// This is Delete Item button Which delete the item from the list
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void DeleteItemButtonClicked(object sender, EventArgs e)
     {
-        if(sender is Button btn && btn.BindingContext is DataFromTheUser item)
+        if (sender is Button btn && btn.BindingContext is DataFromTheUser item)
         {
             DataCollection.Remove(item);
         }
